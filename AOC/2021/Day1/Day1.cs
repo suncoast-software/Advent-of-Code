0376ed22c7182b7bc2364f2edb5e58361922738a
Suncoast Software <53012974+suncoast-software@users.noreply.github.com>
2021-11-28 18:28:01 -0500

    day one

---
+++ AOC/2021/Day1/Day1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC._2021.Day1
+{
+    public class Day1
+    {
+    }
+}
