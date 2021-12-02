29837935027686fdb80464a3c6e83b81d7854dc6
Suncoast Software <53012974+suncoast-software@users.noreply.github.com>
2021-12-02 15:14:50 -0500

    benchmark working

---
+++ AOC/2021/Benchmarks/Day2/Day_Two_Benchmark.cs
@@ -0,0 +1,55 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace AOC._2021.Benchmarks.Day2
+{
+    [MemoryDiagnoser]
+    public class Day_Two_Benchmark: Day
+    {
+        private string _input;
+
+        public Day_Two_Benchmark()
+        {
+            Task.Run(async () =>
+            {
+                 _input = await GetInputForDay(2);
+
+            }).ConfigureAwait(false);
+           
+        }
+
+        [Benchmark]
+        public async Task<int> Solve()
+        {
+           
+            var lines = _input.Split(new[] { "\n", "\r" }, StringSplitOptions.RemoveEmptyEntries);
+            var subDepth = 0;
+            var position = 0;
+            var aim = 0;
+            foreach (var line in lines)
+            {
+                var command = line.Split(" ")[0];
+                var amount = line.Split(" ")[1];
+
+                switch (command)
+                {
+                    case "down":
+                        aim += int.Parse(amount);
+                        break;
+
+                    case "up":
+                        aim -= int.Parse(amount);
+                        break;
+
+                    case "forward":
+                        position += int.Parse(amount);
+                        subDepth += aim * int.Parse(amount);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            return subDepth * position;
+        }
+    }
+}
--- AOC/2021/Program.cs
+++ AOC/2021/Program.cs
@@ -1,15 +1,16 @@
-﻿using AOC._2021.Day2;
+﻿using AOC._2021.Benchmarks.Day2;
+using AOC._2021.Day2;
 using BenchmarkDotNet.Running;
 
 Console.WriteLine("Hello, Advent of Code 2021");
 
-//Day1 benchmark = new Day1();
+Day_Two_Benchmark benchmark = new();
 
-//var results = BenchmarkRunner.Run<Day1>();
+var results = BenchmarkRunner.Run<Day_Two_Benchmark>();
 
 
-Day2 day = new();
-var partOneResult = await day.Solve(2);
+//Day2 day = new();
+//var partOneResult = await day.Solve(2);
 //var partTwoResult = await day.Part_Two();
-Console.WriteLine(partOneResult);
+//Console.WriteLine(partOneResult);
 //Console.WriteLine(partTwoResult);
