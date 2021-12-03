aeeb4ea21884e4885edfe9b5378bf37af3726638
Suncoast Software <53012974+suncoast-software@users.noreply.github.com>
2021-12-03 16:29:58 -0500

    Benchmark Day3 Part 1

---
+++ AOC/2021/Benchmarks/Day3/Day3_Benchmark.cs
@@ -0,0 +1,112 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC._2021.Benchmarks.Day3
+{
+    [MemoryDiagnoser]
+    public class Day3_Benchmark: Day
+    {
+       
+        public int Solve_Part1()
+        {
+            string? _input = "input.txt";
+            var lines = _input.Trim().Split(new[] { "\n", "\r" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            var lineLength = lines[0].Length;
+            var gammaBits = new StringBuilder(lineLength);
+            var epsilonBits = new StringBuilder(lineLength);
+
+            for (int i = 0; i < lineLength; i++)
+            {
+                var mostCommon = MostCommon(i, lines);
+                gammaBits.Append(mostCommon);
+
+                var leastCommon = LeastCommon(i, lines);
+                epsilonBits.Append(leastCommon);
+            }
+
+            var gamma = Convert.ToInt32(gammaBits.ToString(), 2);
+            var epsilon = Convert.ToInt32(epsilonBits.ToString(), 2);
+
+            return gamma * epsilon;
+        }
+
+        [Benchmark]
+        public int Solve_Part2()
+        {
+            string? _input = "input.txt";
+            var lines = _input.Trim().Split(new[] { "\n", "\r" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            int lineLength = lines[0].Length;
+
+            var oxygenNums = lines;
+            for (int i = 0; i < lineLength; i++)
+            {
+                var mostCommon = MostCommon(i, oxygenNums);
+                oxygenNums = oxygenNums.Count switch
+                {
+                    > 1 => oxygenNums.Where(x => x[i] == (mostCommon ?? '1')).ToList(),
+                    _ => oxygenNums
+                };
+            }
+
+            var co2Nums = lines;
+            for (int i = 0; i < lineLength; i++)
+            {
+                var leastCommon = LeastCommon(i, co2Nums);
+                co2Nums = co2Nums.Count switch
+                {
+                    > 1 => co2Nums.Where(x => x[i] == (leastCommon ?? '0')).ToList(),
+                    _ => co2Nums
+                };
+
+            }
+
+            var result = Convert.ToInt32(oxygenNums[0], 2) * Convert.ToInt32(co2Nums[0], 2);
+            return result;
+        }
+
+        private static char? MostCommon(int index, List<string> bits)
+        {
+            var count0 = 0;
+            var count1 = 0;
+            foreach (var item in bits)
+            {
+                switch (item[index])
+                {
+                    case '0':
+                        count0 += 1;
+                        break;
+                    default:
+                        count1 += 1;
+                        break;
+                }
+            }
+
+            return count1 == count0 ? null : count1 > count0 ? '1' : '0';
+        }
+
+        private static char? LeastCommon(int index, List<string> bits)
+        {
+            var count0 = 0;
+            var count1 = 0;
+            foreach (var item in bits)
+            {
+                switch (item[index])
+                {
+                    case '0':
+                        count0 += 1;
+                        break;
+                    default:
+                        count1 += 1;
+                        break;
+                }
+            }
+            return count1 == count0 ? null : count1 > count0 ? '0' : '1';
+        }
+    }
+}
--- AOC/2021/Program.cs
+++ AOC/2021/Program.cs
@@ -1,15 +1,15 @@
-﻿using AOC._2021.Benchmarks.Day2;
+﻿using AOC._2021.Benchmarks.Day3;
 using AOC._2021.Day3;
 using BenchmarkDotNet.Running;
 
-//Day_Two_Benchmark benchmark = new();
+Day3_Benchmark benchmark = new();
 
-//var results = BenchmarkRunner.Run<Day_Two_Benchmark>();
+var results = BenchmarkRunner.Run<Day3_Benchmark>();
 
 
-Day3 day = new();
+//Day3 day = new();
 //var partOneResult = await day.Solve_Part1(3);
-var partTwoResult = await day.Solve_Part2(3);
+//var partTwoResult = await day.Solve_Part2(3);
 //var partTwoResult = await day.Part_Two();
 //Console.WriteLine(partOneResult);
-Console.WriteLine(partTwoResult);
+//Console.WriteLine(partTwoResult);
