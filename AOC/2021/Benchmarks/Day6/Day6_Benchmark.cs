ceb5109ee4ec31081ad9519c0b81770b6a699989
Suncoast Software <53012974+suncoast-software@users.noreply.github.com>
2021-12-06 15:48:18 -0500

    add benchmark day 6 :tada:

---
+++ AOC/2021/Benchmarks/Day6/Day6_Benchmark.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC._2021.Benchmarks.Day6
+{
+    [MemoryDiagnoser]
+    public class Day6_Benchmark: Day
+    {
+        private string? _input;
+
+        public ulong Part_One()
+        {
+            return SolveBase();
+        }
+
+        public ulong Part_Two()
+        {
+            return SolveBase();
+        }
+
+        [Benchmark]
+        public ulong SolveBase()
+        {
+            _input = GetInputForDay(6);
+            Span<ulong> initialCounts = stackalloc ulong[9];
+
+            var totalFish = 0ul;
+            for (var i = 0; i < _input.Length - 1; i += 2)
+            {
+                initialCounts[_input[i] - '0']++;
+                totalFish++;
+            }
+
+            var pivot = 0;
+            for (var i = 1; i <= 80; ++i)// <------- change 80 to 256 for Part Two
+            {
+                var count = initialCounts[pivot];
+                totalFish += count;
+                initialCounts[(pivot + 7) % 9] += count;
+                pivot = (pivot + 1) % 9;
+            }
+
+            return totalFish;
+        }
+    }
+}
--- AOC/2021/Program.cs
+++ AOC/2021/Program.cs
@@ -2,15 +2,18 @@
 //using AOC._2021.Benchmarks.Day5;
 //using AOC._2015.Benchmark.Day5;
 //using AOC._2015.Benchmarks.Day5;
+using AOC._2021.Benchmarks.Day6;
 using AOC._2021.Day6;
+using BenchmarkDotNet.Running;
 //using BenchmarkDotNet.Running;
 
-//Day5_Benchmark benchmark = new();
-//var results = BenchmarkRunner.Run<Day5_Benchmark>();
+Day6_Benchmark benchmark = new();
+var results = BenchmarkRunner.Run<Day6_Benchmark>();
 
-Day6 day = new();
+
+//Day6 day = new();
 //var partOneResult = await day.Part_One();
 //var partTwoResult = await day.Solve_Part2(3);
-var partTwoResult = day.Part_Two();
+//var partTwoResult = day.Part_Two();
 //Console.WriteLine(partOneResult);
-Console.WriteLine(partTwoResult);
+//Console.WriteLine(partTwoResult);
